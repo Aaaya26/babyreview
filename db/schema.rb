8397dd102095b59bccc096123cbb7b89e5e0c377
Aaaya26 <a.spring.spring5@gmail.com>
2024-01-31 15:42:44 +0900

    reviewモデルの導入、マイグレーションファイルの設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_31_051352) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_31_062621) do
+  create_table "reviews", charset: "utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.integer "category_id", null: false
+    t.integer "evaluation", null: false
+    t.text "text", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -26,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_31_051352) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reviews", "users"
 end
