51914f9554245ca19e0a338a47f70be0de6725ab
Aaaya26 <a.spring.spring5@gmail.com>
2024-01-31 15:44:09 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'reviews#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :reviews, only: :index
 end
