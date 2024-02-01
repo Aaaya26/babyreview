820565f499f4dac392211b0fd7bdfb26a8a699bd
Aaaya26 <a.spring.spring5@gmail.com>
2024-02-01 22:03:18 +0900

    コントローラーの設定

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,4 +7,20 @@ class ReviewsController < ApplicationController
   def new
     @review = Review.new
   end
+
+  def create
+    @review = Review.new(review_params)
+    
+    if @review.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:item_name, :category_id, :evaluation_id, :text).merge(user_id: current_user.id)
+  end
 end
