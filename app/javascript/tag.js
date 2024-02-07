959885cae2643cbecc82dd1fcad5bd8330aa484b
Aaaya26 <a.spring.spring5@gmail.com>
2024-02-07 23:53:43 +0900

    非同期通信後の処理の実装

--- app/javascript/tag.js
+++ app/javascript/tag.js
@@ -9,20 +9,23 @@ document.addEventListener("turbo:load", () => {
       XHR.responseType = "json";
       XHR.send();
       XHR.onload = () => {
-        const tagName = XHR.response.keyword;
         const searchResult = document.getElementById("search-result");
-        tagName.forEach((tag) => {
-          const childElement = document.createElement("div");
-          childElement.setAttribute("class", "child");
-          childElement.setAttribute("id", tag.id);
-          childElement.innerHTML = tag.tag_name;
-          searchResult.appendChild(childElement);
-          const clickElement = document.getElementById(tag.id);
-          clickElement.addEventListener("click", () => {
-            document.getElementById("tag-name").value = clickElement.textContent;
-            clickElement.remove();
+        searchResult.innerHTML = "";
+        if (XHR.response) {
+          const tagName = XHR.response.keyword;
+          tagName.forEach((tag) => {
+            const childElement = document.createElement("div");
+            childElement.setAttribute("class", "child");
+            childElement.setAttribute("id", tag.id);
+            childElement.innerHTML = tag.tag_name;
+            searchResult.appendChild(childElement);
+            const clickElement = document.getElementById(tag.id);
+            clickElement.addEventListener("click", () => {
+              document.getElementById("tag-name").value = clickElement.textContent;
+              clickElement.remove();
+            });
           });
-        });
+        };
       };
     });
     };
