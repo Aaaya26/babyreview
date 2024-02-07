03446dfb943dffbf9cf454a153524f3bd06d0b43
Aaaya26 <a.spring.spring5@gmail.com>
2024-02-07 11:10:42 +0900

    ビューの編集

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,11 +2,11 @@ class QuestionsController < ApplicationController
 
   def create
     @quetion = Question.create(question_params)
-    redirect_to review_path
+    redirect_to review_questions_path
   end
 
   private
   def question_params
-    params.requre(:question).permit(:comment).merge(user_id: current_user.id, review_id: params[:review_id])
+    params.require(:question).permit(:comment).merge(user_id: current_user.id, review_id: params[:review_id])
   end
 end
