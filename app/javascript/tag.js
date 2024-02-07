ddfda8dda1b724ff4ace9e7212722ff5a45daf64
Aaaya26 <a.spring.spring5@gmail.com>
2024-02-07 23:33:53 +0900

    タグフォーム内の文字列の取得

--- app/javascript/tag.js
+++ app/javascript/tag.js
@@ -1,6 +1,9 @@
 document.addEventListener("turbo:load", () => {
   const tagNameInput = document.querySelector("#tag-name");
   if (tagNameInput){
-    
+    const inputElement = document.getElementById("tag-name");
+    inputElement.addEventListener("input", () => {
+      const keyword = document.getElementById("tag-name").value;
+    });
     };
   });
