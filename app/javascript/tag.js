fc049451488f1952a94bde6ced9b784e5068fea2
Aaaya26 <a.spring.spring5@gmail.com>
2024-02-07 23:34:51 +0900

    リクエストの定義

--- app/javascript/tag.js
+++ app/javascript/tag.js
@@ -4,6 +4,8 @@ document.addEventListener("turbo:load", () => {
     const inputElement = document.getElementById("tag-name");
     inputElement.addEventListener("input", () => {
       const keyword = document.getElementById("tag-name").value;
+      const XHR = new XMLHttpRequest();
+      XHR.open("GET", `/reviews/search/?keyword=${keyword}`, true);
     });
     };
   });
