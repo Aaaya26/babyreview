81d52d061df1c609cb4ca8366c61ed99464e7596
Aaaya26 <a.spring.spring5@gmail.com>
2024-02-27 14:23:20 +0900

    モデルの追加設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    has_many :reviews
-    has_many :questions
+    has_many :reviews, dependent: :destroy
+    has_many :questions, dependent: :destroy
 
     with_options presence: true do
     validates :name
